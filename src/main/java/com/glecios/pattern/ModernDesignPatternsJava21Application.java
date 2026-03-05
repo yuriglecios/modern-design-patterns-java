a14e17a99bd79cdfba41bb8f276ab6786cc8f2d8
Glecios <glleciosyuri@gmail.com>
2026-03-04 22:11:53 -0300

    Yuri Glecios - Commit Inicial

---
+++ src/main/java/com/glecios/pattern/ModernDesignPatternsJava21Application.java
@@ -0,0 +1,13 @@
+package com.glecios.pattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModernDesignPatternsJava21Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModernDesignPatternsJava21Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/glecios/pattern/ModernDesignPatternsJava21ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glecios.pattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModernDesignPatternsJava21ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
